a5a0bcc5adddf7308dc6650af4fd91d9a539e498
Gasyh3 <kevrakt@gmail.com>
2022-03-11 11:57:57 +0100

    Mise en place de l'email-validator

--- backend/middleware/email-validator.js
+++ backend/middleware/email-validator.js
@@ -2,3 +2,14 @@
  * * Imports
 */
 const verifyEmail = require('email-validator');
+
+/** 
+ * * Exports
+*/
+module.exports = (req, res, next) => {
+    if (!verifyEmail.validate(req.body.email)) {
+        return res.status(400).json({ message: "Saisir une adresse email valide."});
+    } else {
+        next();
+    }
+};

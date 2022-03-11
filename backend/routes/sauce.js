c0e92d53f8e2f025956f45120e7c3a266e07fd35
Gasyh3 <kevrakt@gmail.com>
2022-03-11 18:21:36 +0100

    Mise en place de la route getOneSauce

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -5,13 +5,13 @@ const express = require('express');
 const router = express.Router();
 const sauceControl = require('../controllers/sauce');
 const auth = require('../middleware/auth');
-
 const multer = require('../middleware/multer');
 
 /**
  * * Routes
  */
 router.post('/', auth, multer, sauceControl.createSauce);
+router.get('/:id', auth, sauceControl.getOneSauce);
 
 /**
  * * Export

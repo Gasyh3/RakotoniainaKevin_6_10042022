16793935ce73c46cd90357f39fdabbff2e152d56
Gasyh3 <kevrakt@gmail.com>
2022-03-11 17:36:48 +0100

    Mise en place de la création de sauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -4,3 +4,17 @@
 const Sauce = require('../models/Sauce');
 const fs = require('fs');
 
+/** 
+ * * Créer une sauce
+*/
+exports.createSauce = (req, res, next) => {
+    const sauceObject = JSON.parse(req.body.sauce);
+    delete sauceObject._id;
+    const sauce = new Sauce({
+        ...sauceObject,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    });
+    sauce.save()
+    .then(() => res.status(201).json({message: 'Sauce enregistrée'}))
+    .catch(error => res.status(400).json({error}));
+};

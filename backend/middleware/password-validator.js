ed835a287692554c8cd381d7e100075347503214
Gasyh3 <kevrakt@gmail.com>
2022-03-11 15:36:13 +0100

    Mise en place de password-validator.js

---
+++ backend/middleware/password-validator.js
@@ -0,0 +1,15 @@
+/**
+ * * Imports
+ */
+const passwordSchema = require('../models/Password');
+
+/**
+ * * Exports
+ */
+module.exports = (req, res, next) => {
+    if (!passwordSchema.validate(req.body.password)) {
+        return res.status(400).json({message: 'Le mot de passe doit contenir entre 8 et 100 caractères, avec au minimum une majuscule, une minuscule et 2 chiffres'});
+    } else {
+        next();
+    }
+};

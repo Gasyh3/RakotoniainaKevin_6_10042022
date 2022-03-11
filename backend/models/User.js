64380513e0b28e9e416edf93ad4447912434e320
Gasyh3 <kevrakt@gmail.com>
2022-03-11 10:52:38 +0100

    Mise en place du modèle User + mongoose-unique-validator

---
+++ backend/models/User.js
@@ -0,0 +1,25 @@
+/** 
+ * * Imports
+*/
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+/**
+ * * Modèle utilisateur
+ * * email : String — adresse e-mail de l'utilisateur [unique]
+ * * password : String — mot de passe de l'utilisateur haché
+*/
+const userSchema = mongoose.Schema({
+    email: { type: 'string', required: true, unique: true },
+    password: { type: 'string', required: true }
+});
+
+/**
+ * * Contrôle d'unicité
+ */
+userSchema.plugin(uniqueValidator);
+
+/**
+ * * Export
+ */
+module.exports = mongoose.model('User', userSchema);

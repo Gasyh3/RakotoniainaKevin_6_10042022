7b09066ec74d9161197ed9f7e0cedf584a6445c2
Gasyh3 <kevrakt@gmail.com>
2022-03-11 18:27:15 +0100

    Modifier la sauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -22,8 +22,22 @@ exports.createSauce = (req, res, next) => {
 /**
  * * Interroger une sauce
  */
-exports.getOnesauce = (req, res, next) => {
+exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({_id: req.params.id})
     .then(sauce => res.status(200).json(sauce))
     .catch(error => res.status(404).json({error}));
 };
+
+/**
+ * * Modifier une sauce
+ */
+exports.modifySauce = (req, res, next) => {
+    const sauceObject = req.file ?
+    {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body};
+    Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Sauce modifiée'}))
+    .catch(error => res.status(400).json({ error}));
+}

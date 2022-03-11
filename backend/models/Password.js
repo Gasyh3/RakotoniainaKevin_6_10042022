fea1e8831054c20bbacfe399563b0ed20da36bce
Gasyh3 <kevrakt@gmail.com>
2022-03-11 12:07:59 +0100

    Mise en place des propriétés du mdp

--- backend/models/Password.js
+++ backend/models/Password.js
@@ -2,3 +2,23 @@
  * * Imports
 */
 const verifyPassword = require('password-validator');
+
+const passwordSchema = new verifyPassword();
+
+/** 
+ * * Critères pour avoir un password valide
+*/
+passwordSchema
+    .is().min(8)
+    .is().max(100)
+    .has().uppercase()
+    .has().lowercase()
+    .has().digits(2)
+    .has().not().spaces()
+    .is().not().oneOf(['Motdepasse123', 'Password123']);
+
+
+/** 
+ * * Exports
+*/
+module.exports = passwordSchema;

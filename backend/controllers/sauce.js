51089162165dccd0c49ac2a32806dae172ed1897
Gasyh3 <kevrakt@gmail.com>
2022-03-18 15:24:01 +0100

    Mise en place des paramètres pour like ou dislike + routes

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -66,3 +66,33 @@ exports.getAllSauces = (req, res, next) => {
     .then(sauces => res.status(200).json({sauces}))
     .catch(eror => res.status(400).json({error}));
 };
+
+/**
+ * * Mettre ou supprimer un like ou un dislike
+ */
+ exports.likeSauce = (req, res, next) => {
+    if (req.body.like === 1) {  // J'aime
+        Sauce.updateOne( {_id:req.params.id}, 
+                         { $push: { usersLiked: req.body.userId }, $inc: { likes: +1 } })
+          .then(() => res.status(200).json({ message: 'Like ajouté !'}))
+          .catch(error => res.status(400).json({ error }));
+    } else if (req.body.like === -1) {  // Je n'aime pas
+        Sauce.updateOne( {_id:req.params.id}, { $push: { usersDisliked: req.body.userId }, $inc: { dislikes: +1 } })
+          .then(() => res.status(200).json({ message: 'Dislike ajouté !'}))
+          .catch(error => res.status(400).json({ error }));
+    } else {  // Je n'ai plus d'avis
+        Sauce.findOne({ _id: req.params.id })
+          .then(sauce => {
+            if (sauce.usersLiked.includes(req.body.userId)) {
+              Sauce.updateOne( {_id:req.params.id}, { $pull: { usersLiked: req.body.userId }, $inc: { likes: -1 } })
+                .then(() => res.status(200).json({ message: 'Like supprimé !'}))
+                .catch(error => res.status(400).json({ error }))
+            } else if (sauce.usersDisliked.includes(req.body.userId)) {
+              Sauce.updateOne( {_id:req.params.id}, { $pull: { usersDisliked: req.body.userId }, $inc: { dislikes: -1 } })
+                .then(() => res.status(200).json({ message: 'Dislike supprimé !'}))
+                .catch(error => res.status(400).json({ error }))
+            }
+          })
+          .catch(error => res.status(400).json({ error }));
+    }
+  };
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -15,6 +15,7 @@ router.post('/', auth, multer, sauceControl.createSauce);
 router.get('/:id', auth, sauceControl.getOneSauce);
 router.put('/:id', auth, multer, sauceControl.modifySauce);
 router.delete('/:id', auth, sauceControl.deleteSauce);
+router.post('/:id/like', auth, sauceControl.likeSauce);
 
 /**
  * * Export
